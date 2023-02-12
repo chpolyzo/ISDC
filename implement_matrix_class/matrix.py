b4a2884d6a0e17c298a4898927592b90f0e4f6d6
Chrysanthi Polyzoni <49782010+chpolyzo@users.noreply.github.com>
2023-02-12 19:01:30 +0100

    AssertionError: Error in your __mul__ function

--- implement_matrix_class/matrix.py
+++ implement_matrix_class/matrix.py
@@ -25,11 +25,15 @@ class Matrix(object):
         self.g = grid
         self.h = len(grid)
         self.w = len(grid[0])
-
-    #
-    # Primary matrix math methods
-    #############################
- 
+        
+    def __str__(self):
+        return f"Matrix({self.g})"
+        
+    def is_square(self):
+        return self.h == self.w
+        
+        
+        
     def determinant(self):
         """
         Calculates the determinant of a 1x1 or 2x2 matrix.
@@ -38,18 +42,27 @@ class Matrix(object):
             raise(ValueError, "Cannot calculate determinant of non-square matrix.")
         if self.h > 2:
             raise(NotImplementedError, "Calculating determinant not implemented for matrices largerer than 2x2.")
-        
-        # TODO - your code here
-
+            
+        try:
+            # calculate the determinant of the original 2x2 matrix
+            det = (self.g[0][0] * self.g[1][1] - self.g[1][0] * self.g[0][1])
+        except:
+            # calculate the determinant of the original 1x1 matrix
+            det = self.g[0][0]
+        return det
+    
     def trace(self):
         """
         Calculates the trace of a matrix (sum of diagonal entries).
         """
         if not self.is_square():
             raise(ValueError, "Cannot calculate the trace of a non-square matrix.")
-
-        # TODO - your code here
-
+        
+        tr = sum([self.g[i][j] for i in range(len(self.g)) for j in range(len(self.g[0])) if i == j])
+        
+        return tr
+    
+    
     def inverse(self):
         """
         Calculates the inverse of a 1x1 or 2x2 Matrix.
@@ -58,21 +71,44 @@ class Matrix(object):
             raise(ValueError, "Non-square Matrix does not have an inverse.")
         if self.h > 2:
             raise(NotImplementedError, "inversion not implemented for matrices larger than 2x2.")
-
-        # TODO - your code here
-
+            
+        try:
+            # calculate the adjoint of the original 2x2 matrix
+            adjoint = 1 / (self.g[0][0] * self.g[1][1] - self.g[0][1] * self.g[1][0])
+            # create the swaped grid
+            swap = [[self.g[1][1], -self.g[0][1]],[-self.g[1][0], self.g[0][0]]]
+            # create the inverse list
+            inv = [adjoint * swap[i][j] for i in range(len(swap)) for j in range(len(swap[0]))]
+            # create matrix from grid
+            mtx = Matrix([[inv[0], inv[1]],[inv[2], inv[3]]])
+            
+        except:
+            # calculate inverse of an 1x1 matrix
+            inv = [1 / self.g[0][0]]
+            # create the matrix from the grid
+            mtx = Matrix([inv])
+              
+        return mtx
+    
     def T(self):
         """
         Returns a transposed copy of this Matrix.
         """
-        # TODO - your code here
-
-    def is_square(self):
-        return self.h == self.w
-
-    #
-    # Begin Operator Overloading
-    ############################
+        try:
+            # create the transposed list
+            trs_ls = [self.g[j][i] for i in range(len(self.g[0])) for j in range(len(self.g))]
+            # handle 2x2 matrix
+            trs_mtx = Matrix([[trs_ls[0], trs_ls[1]],[trs_ls[2], trs_ls[3]]])
+        except:
+            # handle 1x1 matrix
+            trs_mtx = Matrix([self.g[0]])
+            
+        
+        return trs_mtx
+    
+    ##############################
+    # Begin Operator Overloading #
+    ##############################
     def __getitem__(self,idx):
         """
         Defines the behavior of using square brackets [] on instances
@@ -88,7 +124,115 @@ class Matrix(object):
           1
         """
         return self.g[idx]
+    
+    def __add__(self,other):
+        """
+        Defines the behavior of the + operator
+        """
+        if self.h != other.h or self.w != other.w:
+            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
 
+        try:
+            # create the expected sum as a lis for 2x2 matrix
+            add_ls = [self.g[i][j] + other.g[i][j] for i in range(len(self.g)) for j in range(len(self.g[0]))]
+            # handle 2x2 matrix
+            mtx_add = Matrix([[add_ls[0], add_ls[1]],[add_ls[2], add_ls[3]]])
+        except:
+            # handle 1x1 matrix
+            mtx_add = Matrix([self.g[0]])
+            
+        return mtx_add
+    
+    def __neg__(self):
+        """
+        Defines the behavior of - operator (NOT subtraction)
+        Example:
+        > my_matrix = Matrix([ [1, 2], [3, 4] ])
+        > negative  = -my_matrix
+        > print(negative)
+          -1.0  -2.0
+          -3.0  -4.0
+        """
+
+        try:
+            # create the expected negative numbers in a list for a 2x2 matrix
+            neg_ls = [float(-self.g[i][j]) for i in range(len(self.g)) for j in range(len(self.g[0]))]
+            # handle 2x2 matrix
+            mtx_neg = Matrix([[neg_ls[0], neg_ls[1]],[neg_ls[2], neg_ls[3]]])
+        except:
+            # handle 1x1 matrix
+            mtx_neg = -self.g[0][0]
+            
+        return mtx_neg
+
+    def __sub__(self,other):
+        """
+        Defines the behavior of the + operator
+        """
+        if self.h != other.h or self.w != other.w:
+            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
+
+        try:
+            # create the expected sum as a lis for 2x2 matrix
+            sub_ls = [self.g[i][j] - other.g[i][j] for i in range(len(self.g)) for j in range(len(self.g[0]))]
+            # handle 2x2 matrix
+            mtx_sub = Matrix([[sub_ls[0], sub_ls[1]],[sub_ls[2], sub_ls[3]]])
+        except:
+            # handle 1x1 matrix
+            mtx_sub = Matrix([self.g[0]])
+            
+        return mtx_sub
+    
+    def dot(self_vector, other_vector):
+        """
+        Defines the behavior of vector multiplication 
+    
+        """
+        dot_ls = sum( [self_vector[i]*other_vector[i] for i in range(len(other_vector))] )
+        
+        return dot_ls
+    
+    def __mul__(self,other):
+        """
+        Defines the behavior of * operator (matrix multiplication)
+        """
+        try:
+            # handle 2x2 matrix
+            otherT = other.T()
+            mul_ls = [Matrix.dot(self.g[i], otherT[j]) for i in range(len(self.g)) for j in range(len(otherT.g[0]))]
+            mtx_mul = Matrix([[mul_ls[0], mul_ls[1]],[mul_ls[2], mul_ls[3]]])
+            
+        except:
+            # handle 1x1 matrix
+            mtx_mul = Matrix.dot(self.g[0], otherT[0])
+        
+        return mtx_mul
+    
+    def __rmul__(self, other):
+        """
+        Called when the thing on the left of the * is not a matrix.
+        Example:
+        > identity = Matrix([ [1,0], [0,1] ])
+        > doubled  = 2 * identity
+        > print(doubled)
+          2.0  0.0
+          0.0  2.0
+        """
+        if isinstance(other, numbers.Number):
+            pass
+        
+            rmul_ls = [other * self[i][j] for i in range(len(self.g)) for j in range (len(self.g[0]))]
+        try:
+            mtx_rmul = Matrix([[rmul_ls[0], rmul_ls[1]],[rmul_ls[2], rmul_ls[3]]])
+            
+        except:
+            mtx_rmul = other * self.g[0][0]
+            
+        
+        return mtx_rmul
+
+
+    
     def __repr__(self):
         """
         Defines the behavior of calling print on an instance of this class.
@@ -98,64 +242,3 @@ class Matrix(object):
             s += " ".join(["{} ".format(x) for x in row])
             s += "\n"
         return s
-
-    def __add__(self,other):
-        """
-        Defines the behavior of the + operator
-        """
-        if self.h != other.h or self.w != other.w:
-            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
-        #   
-        # TODO - your code here
-        #
-
-    def __neg__(self):
-        """
-        Defines the behavior of - operator (NOT subtraction)
-
-        Example:
-
-        > my_matrix = Matrix([ [1, 2], [3, 4] ])
-        > negative  = -my_matrix
-        > print(negative)
-          -1.0  -2.0
-          -3.0  -4.0
-        """
-        #   
-        # TODO - your code here
-        #
-
-    def __sub__(self, other):
-        """
-        Defines the behavior of - operator (as subtraction)
-        """
-        #   
-        # TODO - your code here
-        #
-
-    def __mul__(self, other):
-        """
-        Defines the behavior of * operator (matrix multiplication)
-        """
-        #   
-        # TODO - your code here
-        #
-
-    def __rmul__(self, other):
-        """
-        Called when the thing on the left of the * is not a matrix.
-
-        Example:
-
-        > identity = Matrix([ [1,0], [0,1] ])
-        > doubled  = 2 * identity
-        > print(doubled)
-          2.0  0.0
-          0.0  2.0
-        """
-        if isinstance(other, numbers.Number):
-            pass
-            #   
-            # TODO - your code here
-            #
-            
