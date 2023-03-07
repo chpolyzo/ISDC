89a4699f689a3b158ef238f63bb49acfd6669905
Chrysanthi Polyzoni <49782010+chpolyzo@users.noreply.github.com>
2023-03-07 18:31:00 +0100

    Working with c++ and vectors

---
+++ 2D_filters/Translate_Python_Cpp/vectors/Vectors/DeclaringVectors.cpp
@@ -0,0 +1,18 @@
+// TODO: import the vector library
+#include <vector>
+
+
+// TODO: write a program that declares three integer vectors named:
+//       vector1
+//       vector2
+//       vector3
+int DeclaringVectors() {
+
+    std::vector<int> vector1;
+    std::vector<int> vector2;
+    std::vector<int> vector3;
+
+    return 0;
+}
+// g++ main.cpp
+// . / a.out
---
+++ 2D_filters/Translate_Python_Cpp/vectors/Vectors/NameSpace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+// TODO: Use the standard namespace
+
+using namespace std;
+
+int NameSpace() {
+
+    string fruit = "apple";
+    string vegetable = "broccoli";
+
+    cout << "My favorite fruit is " << fruit << " "
+        "and my favorite vegetable is " << vegetable << "\n";
+
+    return 0;
+}
